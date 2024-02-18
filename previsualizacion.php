89a7ddcef93758bf632eabb2b9beafd6e4af0304
Esthertxo <146742505+DAW204@users.noreply.github.com>
2024-02-18 12:48:49 +0100

    Gestion de sesion añadida en previsualizacion

--- previsualizacion.php
+++ previsualizacion.php
@@ -32,86 +32,79 @@ and open the template in the editor.
         </style>
     </head>
     <body>
-
-        <h1>ERES INVITADO</h1>
-        <h2>Catalogo de libros</h2>
         <?php
-        /* inicio sesion */
-        session_start();
+        if (isset($_SESSION['usuario']) && isset($_SESSION['rol']) == 'invitado') {
+            ?>
 
-        /* Inlcuimos la conexion a la BD */
-        include 'conexion.php';
-
-        // Obtenemos la conexión utilizando la función getConn() (definida en el php de conexion a la BD)
-        $conexion = getConn();
-        $consultaGeneral = "select * from libro;";
-        $resultado = mysqli_query($conexion, $consultaGeneral)
-                or die("Fallo en la consulta");
-        ?>
-        <table>
-            <tr>
-                <th>Titulo</th>
-                <th>Autor</th>
-                <th>precio</th>
-                <th>Editorial</th>
-            </tr>
-
+            <h1>ERES INVITADO</h1>
+            <h2>Catalogo de libros</h2>
             <?php
-            while ($row = mysqli_fetch_assoc($resultado))
-            {
-                ?>
+            /* inicio sesion */
+            session_start();
 
+            /* Inlcuimos la conexion a la BD */
+            include 'conexion.php';
+
+            // Obtenemos la conexión utilizando la función getConn() (definida en el php de conexion a la BD)
+            $conexion = getConn();
+            $consultaGeneral = "select * from libro;";
+            $resultado = mysqli_query($conexion, $consultaGeneral)
+                    or die("Fallo en la consulta");
+            ?>
+            <table>
                 <tr>
-                    <td><?php echo $row['titulo'] ?></td>
-                    <td><?php echo $row['autor'] ?></td>
-                    <td><?php echo $row['precio'] ?></td>
-                    <td><?php echo $row['editorial'] ?> </td>
+                    <th>Titulo</th>
+                    <th>Autor</th>
+                    <th>precio</th>
+                    <th>Editorial</th>
                 </tr>
 
+                <?php
+                while ($row = mysqli_fetch_assoc($resultado)) {
+                    ?>
 
-<?php } ?>
-
-        </table>
-
-        <form action="previsualizacion.php" method="POST">
-            <input type="submit" name="solicitar" value="CAMBIAR ROL" />
-            <input type="submit" name="volver" value="LOGIN" />
-        </form>
-
-
-        <?php
-        /* Recojemos el id del usuario de la variable de sesion */
-        $id_usuario = $_SESSION['id_usuario'];
-
-        /* Si el usuario invitado pulsa solicitar, entra en el if y se realiza un insert en la tabla de solicitudes, donde quedara 
-          su peticion de cambio de rol registrada con el estado en pendiente para que despues el vendedor efectue el cambio a comprador */
-        if (isset($_POST['solicitar']))
-        {
-
-            $consulta = "INSERT INTO solicitudes (id_usuario) VALUES ($id_usuario);";
-
-            $consulta = mysqli_query($conexion, $consulta)
-                    or die("Fallo en la consulta");
-//            
-//           
-//            /*MIRAR EN OTRO MOMENTO*/
-//            if ($consulta)
-//            {
-//                
-//                echo "Se ha realizado la solicitud correctamente.";
-//                
-//            }else
-//            {
-//                die("Fallo en la consulta: " . mysqli_error($conexion));
-//            }
-        }
+                    <tr>
+                        <td><?php echo $row['titulo'] ?></td>
+                        <td><?php echo $row['autor'] ?></td>
+                        <td><?php echo $row['precio'] ?></td>
+                        <td><?php echo $row['editorial'] ?> </td>
+                    </tr>
 
 
-        /* Si el usuario invitado pulsa el boton para cambiar de rol le redirigira al login */
-        if (isset($_POST['volver']))
-        {
+                <?php } ?>
 
-            /* Redirigimos a la página especificada, en este caso el login */
+            </table>
+
+            <form action="previsualizacion.php" method="POST">
+                <input type="submit" name="solicitar" value="CAMBIAR ROL" />
+                <input type="submit" name="volver" value="LOGIN" />
+            </form>
+
+
+            <?php
+            /* Recojemos el id del usuario de la variable de sesion */
+            $id_usuario = $_SESSION['id_usuario'];
+
+            /* Si el usuario invitado pulsa solicitar, entra en el if y se realiza un insert en la tabla de solicitudes, donde quedara 
+              su peticion de cambio de rol registrada con el estado en pendiente para que despues el vendedor efectue el cambio a comprador */
+            if (isset($_POST['solicitar'])) {
+
+                $consulta = "INSERT INTO solicitudes (id_usuario) VALUES ($id_usuario);";
+
+                $consulta = mysqli_query($conexion, $consulta)
+                        or die("Fallo en la consulta");
+            }
+
+
+            /* Si el usuario invitado pulsa el boton para cambiar de rol le redirigira al login */
+            if (isset($_POST['volver'])) {
+
+                /* Redirigimos a la página especificada, en este caso el login */
+                header("Location: login.php");
+                exit;
+            }
+        } else {
+            session_destroy();
             header("Location: login.php");
             exit;
         }
